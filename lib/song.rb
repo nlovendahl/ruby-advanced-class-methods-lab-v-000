c90b443526b2e51f9bc3ec6e7d978d0b7ec3da75
Nisse Lovendahl <nisselovendahl@gmail.com>
2018-10-17 15:53:39 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,13 +1,17 @@
 class Song
   attr_accessor :name, :artist_name
   @@all = []
- 
+
   def self.all
     @@all
   end
- 
+
+  def self.create
+    self.all << self
+  end
+
   def save
     self.class.all << self
   end
- 
+
 end
