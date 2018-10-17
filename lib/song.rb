afad4ae477eca96838ed7e67aff8537f1dd4aabb
Nisse Lovendahl <nisselovendahl@gmail.com>
2018-10-17 16:20:54 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -30,7 +30,7 @@ class Song
   end
 
   def self.find_or_create_by_name(name)
-    if self.all.include(name)
+    if self.all.include?(name)
       find_by_name(name)
     else
       create_by_name
