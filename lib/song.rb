656e84267bcd4e01d62102f5e234cf048f4c8930
Nisse Lovendahl <nisselovendahl@gmail.com>
2018-10-17 16:13:56 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -15,12 +15,11 @@ class Song
   def self.new_by_name(name)
     song = self.new
     song.name = name
-    self.all << song
     song
   end
 
   def self.create_by_name(name)
-    
+
 
   def save
     self.class.all << self
